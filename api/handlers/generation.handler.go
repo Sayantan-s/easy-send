1e08b307c9c66299650ad5862bb493230a78763d
Sayantan Samanta <sssamanta789@gmail.com>
2023-11-27 20:42:26 +0530

    feat: transcription api to assembly ai

--- api/handlers/generation.handler.go
+++ api/handlers/generation.handler.go
@@ -2,18 +2,50 @@ package handlers
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 
 	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 	"github.com/gofiber/fiber/v2"
 	"github.com/sayantan-s/easy-send/integrations/cloudinary"
+	"github.com/sayantan-s/easy-send/integrations/openai"
 	"github.com/sayantan-s/easy-send/utils/res"
 )
 
+type SuccessTemplateResponse struct{
+	TranscriptionId string
+	RecordedUrl string
+}
+
 
 func GenerateTranscriptCVS(c *fiber.Ctx) error{
+
+	uploadDir := "./assets"
+	if err := os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+		return res.Failure(c, res.FalureTemplate{
+			StatusCode: fiber.StatusInternalServerError,
+			Message: "Unable to process File",
+		})
+	}
+	form, err := c.MultipartForm()
+	if err != nil {
+		return res.Failure(c, res.FalureTemplate{
+			StatusCode: fiber.StatusInternalServerError,
+			Message: "Unable to process File",
+		})
+	}
+	file := form.File["file"][0]
+	uploadPath := filepath.Join(uploadDir, file.Filename)
+	
+	if err := c.SaveFile(file, uploadPath); err != nil {
+		return res.Failure(c, res.FalureTemplate{
+			StatusCode: fiber.StatusInternalServerError,
+			Message: "Unable to process file",
+		})
+	}
+
 	fileHeader, _ := c.FormFile("file")
 	fileDoc, _ := fileHeader.Open()
-
 	ctx := context.Background()
 	reposnse, err := cloudinary.GetCient().Upload.Upload(ctx, fileDoc, uploader.UploadParams{
 		Folder: "media",
@@ -25,9 +57,15 @@ func GenerateTranscriptCVS(c *fiber.Ctx) error{
 			Message: "Unable to upload file",
 		})
 	}
+	transcriptionId, err := openai.Completions(uploadPath)
+	os.Remove(uploadPath)
+	
 	return res.Success(c, res.SuccessTemplate{
 		StatusCode: fiber.StatusCreated,
 		Message: "successfully generated LinkedIn messages",
-		Data: reposnse.SecureURL,
+		Data: SuccessTemplateResponse{
+			TranscriptionId: transcriptionId,
+			RecordedUrl: reposnse.SecureURL,
+		},
 	})
 }
--- api/integrations/openai/openai.go
+++ api/integrations/openai/openai.go
@@ -1,16 +1,79 @@
 package openai
 
-const API_KEY = "YOUR_API_KEY"
-const UPLOAD_URL = "https://api.assemblyai.com/v2/upload"
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 
-func uploadFileToAssemblyAI(){
-   // Setup HTTP client and set header
-    // client := &http.Client{}
-    // req, _ := http.NewRequest("POST", UPLOAD_URL, bytes.NewBuffer(data))
-    // req.Header.Set("authorization", API_KEY)
-    // res, err := client.Do(req)
+	"github.com/sayantan-s/easy-send/config"
+)
+
+const UPLOAD_URL = "https://api.assemblyai.com/v2/upload"
+const TRANSCRIPT_URL = "https://api.assemblyai.com/v2/transcript"
+
+
+func uploadFileToAssemblyAI(path string) (string,error){
+    data, err := ioutil.ReadFile(path)
+    API_KEY := config.GetConfig("AAI_API_KEY")
+
+    if err != nil {
+        return "", err
+    }
+    
+    client := &http.Client{}
+    req, _ := http.NewRequest("POST", UPLOAD_URL, bytes.NewBuffer(data))
+    req.Header.Set("authorization", API_KEY)
+    res, err := client.Do(req)
+
+    if err != nil {
+        return "", err
+    }
+
+    var result map[string]string
+    json.NewDecoder(res.Body).Decode(&result)
+
+    return result["upload_url"], nil
 }
 
-func Completetions(){
+func startComputingTranscriptions(url string)(string, error){
+    values := map[string]string{"audio_url": url}
+    jsonData, err := json.Marshal(values)
+    if err != nil {
+        return "", err
+    }
+    API_KEY := config.GetConfig("AAI_API_KEY")
+    client := &http.Client{}
+    req, _ := http.NewRequest("POST", TRANSCRIPT_URL, bytes.NewBuffer(jsonData))
+    req.Header.Set("content-type", "application/json")
+    req.Header.Set("authorization", API_KEY)
+    res, err := client.Do(req)
 
+    if err != nil {
+       return "", err
+    }
+
+    defer res.Body.Close()
+
+    if err != nil {
+       return "", err
+    }
+
+    var result map[string]string
+    json.NewDecoder(res.Body).Decode(&result)
+
+    transcriptionId := result["id"]
+
+    return transcriptionId, nil
+
+}
+
+func Completions(path string)(string, error){
+    uploadUrl, err := uploadFileToAssemblyAI(path);if err != nil{
+        return "", err
+    }
+    transcriptionId, err := startComputingTranscriptions(uploadUrl);if err != nil{
+        return "", err
+    }
+   return transcriptionId, nil
 }
