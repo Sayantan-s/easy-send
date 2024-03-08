c99ca2f6e48a2f352ff4bcaa5656ddf6c5814420
Sayantan Samanta <sssamanta789@gmail.com>
2024-03-08 23:46:21 +0530

    feat: qr code product test

--- client/src/app/generate/page.tsx
+++ client/src/app/generate/page.tsx
@@ -1,5 +1,19 @@
-const GeneratePdf = () => {
-  return <div>Generate</div>;
+"use client";
+import QRCode from "react-qr-code";
+
+const GenerateLink = () => {
+  return (
+    <div
+      style={{ height: "auto", margin: "0 auto", maxWidth: 400, width: "100%" }}
+    >
+      <QRCode
+        value="https://c9c5-2409-40f2-1041-f519-f5be-b512-7b31-4604.ngrok-free.app/generate/api"
+        size={500}
+        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+        viewBox={`0 0 256 256`}
+      />
+    </div>
+  );
 };
 
-export default GeneratePdf;
+export default GenerateLink;
