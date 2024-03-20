5c5f4ec31e468b771d06a2fe60c67da50d600633
Sayantan Samanta <sssamanta789@gmail.com>
2024-03-21 01:37:04 +0530

    feat(cli): new changes

---
+++ cli/target/debug/cli.d
@@ -0,0 +1,1 @@
+/Users/sayantansamanta/Documents/work/easy-send/cli/target/debug/cli: /Users/sayantansamanta/Documents/work/easy-send/cli/src/main.rs
---
+++ cli/target/debug/deps/cli-e144bc3d2c489761.d
@@ -0,0 +1,5 @@
+/Users/sayantansamanta/Documents/work/easy-send/cli/target/debug/deps/cli-e144bc3d2c489761: src/main.rs
+
+/Users/sayantansamanta/Documents/work/easy-send/cli/target/debug/deps/cli-e144bc3d2c489761.d: src/main.rs
+
+src/main.rs:
