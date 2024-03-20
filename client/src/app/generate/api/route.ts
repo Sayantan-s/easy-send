5e8c6e8ea0b5b69306fd7e4d82ae072aca563fe6
Sayantan Samanta <sssamanta789@gmail.com>
2024-03-21 01:37:28 +0530

    feat(cli): new changes

--- client/src/app/generate/api/route.ts
+++
@@ -1,9 +0,0 @@
-import Communication from "@/services/Communication";
-import { NextApiHandler } from "next";
-
-export const GET: NextApiHandler = (req, res) => {
-  return Communication.response.success({
-    status: 200,
-    data: "Hello world",
-  });
-};
